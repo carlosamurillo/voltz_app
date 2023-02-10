0f47ea8b8bd0a03133c349a8b204a922f7bd6834
Carlos Alberto Murillo <carlosalbertomurillo@gmail.com>
2023-02-10 13:21:14 -0500

    se agrega overlay support

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:flutter_segment/flutter_segment.dart';
 import 'package:maketplace/keys_model.dart';
 import 'package:maketplace/utils/custom_colors.dart';
 import 'package:stacked_services/stacked_services.dart';
-
+import 'package:overlay_support/overlay_support.dart';
 import 'app/app.locator.dart';
 import 'app/app.router.dart';
 import 'cart/cart_view.dart';
@@ -44,34 +44,36 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      navigatorObservers: [
-        SegmentObserver(),
-      ],
-      title: 'Voltz - Cotización',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        primaryColor: CustomColors.safeBlue,
-        fontFamily: "Hellix",
-      ),
-      //home: QuoteView(quoteId: quoteId, version: version,),
-      home: CartView(
-        quoteId: quoteId,
-        version: version,
-      ),
-      navigatorKey: StackedService.navigatorKey,
-      // home: AddCardView(), // Used when testing a view
-      //initialRoute: Routes.cartView,
-      onGenerateRoute: StackedRouter().onGenerateRoute,
+    return OverlaySupport(
+        child: MaterialApp(
+          navigatorObservers: [
+            SegmentObserver(),
+          ],
+          title: 'Voltz - Cotización',
+          theme: ThemeData(
+            // This is the theme of your application.
+            //
+            // Try running your application with "flutter run". You'll see the
+            // application has a blue toolbar. Then, without quitting the app, try
+            // changing the primarySwatch below to Colors.green and then invoke
+            // "hot reload" (press "r" in the console where you ran "flutter run",
+            // or simply save your changes to "hot reload" in a Flutter IDE).
+            // Notice that the counter didn't reset back to zero; the application
+            // is not restarted.
+            primarySwatch: Colors.blue,
+            primaryColor: CustomColors.safeBlue,
+            fontFamily: "Hellix",
+          ),
+          //home: QuoteView(quoteId: quoteId, version: version,),
+          home: CartView(
+            quoteId: quoteId,
+            version: version,
+          ),
+          navigatorKey: StackedService.navigatorKey,
+          // home: AddCardView(), // Used when testing a view
+          //initialRoute: Routes.cartView,
+          onGenerateRoute: StackedRouter().onGenerateRoute,
+        );
     );
   }
 }
